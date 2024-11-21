2fa8126b6143e941b8ac63a8768b9fba1cc3bc30
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-21 10:50:45 -0500

    Updates were made to play.js file

--- play.js
+++ play.js
@@ -1,20 +1,19 @@
-const net = require("net");
+const connect = require("./client");
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost",
+//     port: 50541,
+//   });
+//   conn.setEncoding("utf8");
+//   conn.on("connect", () => {
+//     console.log("Connection is made successfully");
+//   });
+//   conn.on("data", (data) => {
+//     console.log(data);
+//   });
 
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    console.log("Connection is made successfully");
-  });
-  conn.on("data", (data) => {
-    console.log(data);
-  });
-
-  return conn;
-};
+//   return conn;
+// };
 console.log("Connecting ...");
 connect();
 
