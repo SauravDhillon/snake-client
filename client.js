53cb60c426999544e65ec8e40ec1de6ad1a10fa0
Saurav Dhillon <souravdhillon010@gmail.com>
2024-11-21 20:27:07 -0500

    Updates were made to client.js file

--- client.js
+++ client.js
@@ -1,9 +1,7 @@
 const net = require("net");
+const  { host, PORT , name}  = require("./constants");
 const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541,
-  });
+  const conn = net.createConnection(PORT, host);
   conn.setEncoding("utf8");
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
